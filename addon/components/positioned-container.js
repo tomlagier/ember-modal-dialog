64d4039906c0caab002c41706ab1f9dbb139ee5e
Tom Lagier <lagierta@gmail.com>
2017-03-30 18:04:18 -0700

    Update positioned-container.js

--- addon/components/positioned-container.js
+++ addon/components/positioned-container.js
@@ -25,7 +25,7 @@ export default Ember.Component.extend({
     return targetAttachment === 'center' || targetAttachment === 'middle center';
   }),
 
-  didGetPositioned: observer('isPositioned', on('didInsertElement', function() {
+  didGetPositioned: observer('isPositioned', on('willUpdate', function() {
     if (this._state !== 'inDOM') {
       return;
     }
